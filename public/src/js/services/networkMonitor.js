27baa634b306225f080ab3cb96f555764f4e4db4
4miners <mariusz@serek.net>
2016-10-16 05:21:47 +0200

    Fixed insecure content warning
    {s}.tile.openstreetmap.org is proper site for serving maps over https. Also // for use protocol of the current page.

--- public/src/js/services/networkMonitor.js
+++ public/src/js/services/networkMonitor.js
@@ -106,7 +106,7 @@ var NetworkMap = function () {
 
     L.Icon.Default.imagePath = '/img/leaflet';
 
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    L.tileLayer('//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
 
