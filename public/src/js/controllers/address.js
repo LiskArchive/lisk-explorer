70cfad2c2a7f4cba5da9e0a87f757f2978fbec69
4miners <mariusz@serek.net>
2017-03-08 05:52:33 +0100

    Fixed intend

--- public/src/js/controllers/address.js
+++ public/src/js/controllers/address.js
@@ -2,32 +2,32 @@
 
 angular.module('lisk_explorer.address').controller('AddressController',
 	function ($scope, $rootScope, $routeParams, $location, $http, addressTxs) {
-			$scope.getAddress = function () {
-					$http.get('/api/getAccount', {
-							params: {
-									address: $routeParams.address
-							}
-					}).then(function (resp) {
-							if (resp.data.success) {
-									$scope.address = resp.data;
-							} else {
-									throw 'Account was not found!';
-							}
-					}).catch(function (error) {
-							$location.path('/');
-					});
-			};
-
-			$scope.address = {
+		$scope.getAddress = function () {
+			$http.get('/api/getAccount', {
+				params: {
 					address: $routeParams.address
-			};
+				}
+			}).then(function (resp) {
+				if (resp.data.success) {
+						$scope.address = resp.data;
+				} else {
+						throw 'Account was not found!';
+				}
+			}).catch(function (error) {
+				$location.path('/');
+			});
+		};
 
-			// Sets the filter for which transactions to display
-			$scope.filterTxs = function(direction) {
-				$scope.direction = direction;
-				$scope.txs = addressTxs($routeParams.address, direction);
-			};
+		$scope.address = {
+			address: $routeParams.address
+		};
 
-			$scope.getAddress();
-			$scope.txs = addressTxs($routeParams.address);
+		// Sets the filter for which transactions to display
+		$scope.filterTxs = function(direction) {
+			$scope.direction = direction;
+			$scope.txs = addressTxs($routeParams.address, direction);
+		};
+
+		$scope.getAddress();
+		$scope.txs = addressTxs($routeParams.address);
 	});
