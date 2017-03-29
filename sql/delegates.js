17feba45dd716039ed6e9aa78bfead71e897bb16
Oliver Beddows <oliver@lisk.io>
2017-03-29 23:34:37 +0200

    Standardising code

--- sql/delegates.js
+++ sql/delegates.js
@@ -1,24 +1,24 @@
 'use strict';
 
-var squel = require ('squel').useFlavour ('postgres');
+var squel = require('squel').useFlavour('postgres');
 
 var DelegatesSql = {
-	getLastDelegateBlocks: function (params) {
-		return squel.select()
-			.from('mem_accounts', 'a')
-			.field(squel.select()
-				.from('blocks', 'b')
-				.field('ARRAY[b.timestamp, b.height]')
-				.where('b."generatorPublicKey" = a."publicKey"')
-				.order('b.height', false)
-				.limit(1)
-			, 'block')
-			.field('a.address', 'address')
-			.where('a.address IN ?', params.delegates)
-			.where('a."isDelegate" = 1')
-			.limit(params.delegates.length)
-			.toString();
-	}
+    getLastDelegateBlocks: function (params) {
+        return squel.select()
+            .from('mem_accounts', 'a')
+            .field(squel.select()
+                .from('blocks', 'b')
+                .field('ARRAY[b.timestamp, b.height]')
+                .where('b."generatorPublicKey" = a."publicKey"')
+                .order('b.height', false)
+                .limit(1)
+            , 'block')
+            .field('a.address', 'address')
+            .where('a.address IN ?', params.delegates)
+            .where('a."isDelegate" = 1')
+            .limit(params.delegates.length)
+            .toString();
+    }
 };
 
 module.exports = DelegatesSql;
--- sql/index.js
+++ sql/index.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = {
-	delegates: require('./delegates.js')
+    delegates: require('./delegates.js')
 };
