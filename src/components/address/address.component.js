2d0db7300b82a6d27944a313aceefb102181a006
reyraa <ali@lightcurve.io>
2017-07-31 18:12:08 +0200

    Implement the advanced search box componen

--- src/components/address/address.component.js
+++ src/components/address/address.component.js
@@ -27,11 +27,39 @@ const AddressConstructor = function ($rootScope, $stateParams, $location, $http,
     // Sets the filter for which transactions to display
     vm.filterTxs = direction => {
         vm.direction = direction;
-        vm.txs = addressTxs($stateParams.address, direction);
+        vm.txs = addressTxs({ address: $stateParams.address, direction });
     };
 
+    vm.searchParams = [];
+
+    vm.availableSearchParams = [
+        { key: "senderId", name: "Sender", placeholder: "Sender..." },
+        { key: "recipientId", name: "Recipient", placeholder: "Recipient..." },
+        { key: "amount", name: "Amount", placeholder: "Amount..." },
+        { key: "minAmount", name: "Min", placeholder: "Min Amount..." },
+        { key: "maxAmount", name: "Max", placeholder: "Max A mount..." },
+        { key: "type", name: "Type", placeholder: "Type...", allowMultiple: true }
+    ];
+
+    $rootScope.$on('advanced-searchbox:modelUpdated', function (event, model) {
+        const params = {};
+        Object.keys(model).forEach((key) => {
+            if (model[key]) {
+                params[key] = model[key];
+            }
+            if (key === 'minAmount' || key === 'maxAmount') {
+                params[key] *= 1e8;
+            }
+        });
+
+        if (Object.keys(params).length > 0) {
+            vm.txs = addressTxs(params);
+            vm.txs.loadData();
+        }
+    });
+
     vm.getAddress();
-    vm.txs = addressTxs($stateParams.address);
+    vm.txs = addressTxs({ address: $stateParams.address });
 };
 
 AppAddress.component('address', {
