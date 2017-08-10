027d452b573c16ba4124547d71026f6ba698acc9
jestersimpps <jestersimpps@gmail.com>
2017-08-10 18:27:43 +0200

    dummy tests

--- src/components/404/404.component.spec.js
+++ src/components/404/404.component.spec.js
@@ -1,6 +1,5 @@
 describe('404 Component', function() {
-    it('has a dummy spec to test 2 + 2', function() {
-        // An intentionally failing test. No code within expect() will never equal 4.
-        expect().toEqual(4);
+    it('has a dummy failing spec test', function() {
+        expect(true).toEqual(false);
     });
 });
