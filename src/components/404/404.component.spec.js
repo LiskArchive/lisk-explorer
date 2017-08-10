4a7416f276bf6bed8ed0ed0160ee3e93724de966
jestersimpps <jestersimpps@gmail.com>
2017-08-10 18:19:17 +0200

    karma config

---
+++ src/components/404/404.component.spec.js
@@ -0,0 +1,6 @@
+describe('404 Component', function() {
+    it('has a dummy spec to test 2 + 2', function() {
+        // An intentionally failing test. No code within expect() will never equal 4.
+        expect().toEqual(4);
+    });
+});
