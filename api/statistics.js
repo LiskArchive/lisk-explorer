8179b0ba6ffe0c3e172f596a9ed2b0d371d71f19
reyraa <ali@lightcurve.io>
2017-09-06 12:03:51 +0200

    Modify statistics

--- api/statistics.js
+++ api/statistics.js
@@ -1,23 +1,16 @@
 const statistics = require('../lib/api/statistics');
-// const handler = require('./handler');
+const handler = require('./handler');
 
 module.exports = function (app) {
 	const statisticsApi = new statistics(app);
 
-	const handler = (api, endpoint, req, res) => {
-		api[endpoint].call(
-			api,
-			(data) => { res.json(data); },
-			(data) => { res.json(data); });
-	};
+	app.get('/api/statistics/getLastBlock', (req, res, next) =>
+		handler(statisticsApi, 'getLastBlock', req, res, next));
 
-	app.get('/api/statistics/getLastBlock', (req, res) =>
-		handler(statisticsApi, 'getLastBlock', res));
+	app.get('/api/statistics/getBlocks', (req, res, next) =>
+		handler(statisticsApi, 'getBlocks', req, res, next));
 
-	app.get('/api/statistics/getBlocks', (req, res) =>
-		handler(statisticsApi, 'getBlocks', res));
-
-	app.get('/api/statistics/getPeers', (req, res) =>
-		handler(statisticsApi, 'getPeers', res));
+	app.get('/api/statistics/getPeers', (req, res, next) =>
+		handler(statisticsApi, 'getPeers', req, res, next));
 };
 
