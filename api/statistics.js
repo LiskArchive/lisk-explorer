0aa50111ea2c9dafa2bc98f96c8a3ffa5d9be2a8
reyraa <ali@lightcurve.io>
2017-09-09 10:31:38 +0200

    Fixed parameters of statistics

--- api/statistics.js
+++ api/statistics.js
@@ -5,12 +5,12 @@ module.exports = function (app) {
 	const statisticsApi = new statistics(app);
 
 	app.get('/api/statistics/getLastBlock', (req, res, next) =>
-		handler(statisticsApi, 'getLastBlock', req, res, next));
+		handler(statisticsApi, 'getLastBlock', undefined, req, res, next));
 
 	app.get('/api/statistics/getBlocks', (req, res, next) =>
-		handler(statisticsApi, 'getBlocks', req, res, next));
+		handler(statisticsApi, 'getBlocks', undefined, req, res, next));
 
 	app.get('/api/statistics/getPeers', (req, res, next) =>
-		handler(statisticsApi, 'getPeers', req, res, next));
+		handler(statisticsApi, 'getPeers', undefined, req, res, next));
 };
 
