c857d6af0cdf12996007a2742917a8e63d40279a
Lucas Silvestre <lukas.silvestre@gmail.com>
2018-04-12 14:07:32 +0200

    Use radis for caching known addresses

--- utils/knownAddresses.js
+++ utils/knownAddresses.js
@@ -13,6 +13,8 @@
  * Removal or modification of this copyright notice is prohibited.
  *
  */
+const config = require('../config');
+const client = require('../redis')(config);
 const logger = require('./logger');
 
 module.exports = function () {
@@ -52,10 +54,45 @@ module.exports = function () {
 			return null;
 		};
 
+		// Entry expires every day (86400 seconds) in case new delegate is registered
+		this.setAddress = (account) => {
+			if (!account || !account.address) return false;
+
+			let value = {};
+			if (account.username) {
+				value = { owner: account.username };
+			} else if (account.delegate && account.delegate.username) {
+				value = { owner: account.delegate.username };
+			}
+
+			client.hmset(`address:${account.address}`, value);
+			client.expire(`address:${account.address}`, 86400);
+			return true;
+		};
+
+		this.getAddress = (address, callback) => client.hgetall(`address:${address}`, callback);
+
+		this.isAddressCached = (address, callback) => client.exists(`address:${address}`, callback);
+
+		this.getOrSetKnowledge = (account, callback) => {
+			this.getAddress(account.address, (err, reply) => {
+				if (reply) {
+					callback(err, reply);
+				} else {
+					this.setAddress(account);
+					this.getAddress(account.address, callback);
+				}
+			});
+		};
+
 		this.load = () => {
 			try {
 				logger.info('KnownAddresses:', 'Loading known addresses...');
 				this.addresses = require('../known.json');
+
+				this.addresses.keys.array.forEach((address) => {
+					client.hmset(`address:${address}`, this.addresses[address]);
+				});
 			} catch (err) {
 				logger.error('KnownAddresses:', 'Error loading known.json:', err.message);
 				this.addresses = {};
