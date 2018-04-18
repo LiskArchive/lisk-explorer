7d08bd8a03ff6a32d2b72dcefaa43cb470b1c646
Michal Tuleja <michal@lightcurve.io>
2018-04-18 15:27:59 +0200

    Add abstraction layer for generic HTTP and Lisk Core requests

---
+++ utils/http.js
@@ -0,0 +1,38 @@
+/*
+ * LiskHQ/lisk-explorer
+ * Copyright © 2018 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+
+const request = require('request');
+const HttpStatus = require('http-status-codes');
+
+const validateHttpResponse = (response) => {
+	if (response.statusCode === HttpStatus.OK) return true;
+	if (response.statusCode === HttpStatus.NOT_MODIFIED) return true;
+	return false;
+};
+
+const get = url => new Promise((resolve, reject) => {
+	request.get({
+		url,
+		json: true,
+	}, (err, response, body) => {
+		if (err || !validateHttpResponse(response)) {
+			return reject(err || 'Response was unsuccessful');
+		}
+		return resolve(body);
+	});
+});
+
+module.exports = { get };
