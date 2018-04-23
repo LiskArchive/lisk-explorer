440ff2ad35e7ccda250129461ac55ca842f4cbce
Lucas Silvestre <lukas.silvestre@gmail.com>
2018-04-23 16:58:16 +0200

    Code cleanup and refactoring

--- src/filters/tx-sender.js
+++ src/filters/tx-sender.js
@@ -16,6 +16,5 @@
 import AppFilters from './filters.module';
 
 AppFilters.filter('txSender', () => tx =>
-	(tx.senderDelegate && tx.senderDelegate.username) ||
 	tx.senderUsername || (tx.knownSender && tx.knownSender.owner) ||
 	tx.senderId);
