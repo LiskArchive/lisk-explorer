26e4820c3aad496d5fd2fc855e599646c82e35de
Michal Tuleja <michal@lightcurve.io>
2018-05-14 16:33:41 +0200

    New mobile home page / blocks table layout

--- src/filters/middle-ellipsis.js
+++ src/filters/middle-ellipsis.js
@@ -15,5 +15,9 @@
  */
 import AppFilters from './filters.module';
 
-AppFilters.filter('middleEllipsis', () => (str, visibleDigits) =>
-	`${str.substr(0, visibleDigits)}...${str.substr(str.length - visibleDigits)}`);
+AppFilters.filter('middleEllipsis', () => (str, maxVisibleDigits) => {
+	if (typeof str !== 'string') return '';
+	if (str.length <= (maxVisibleDigits + 1)) return str;
+	const visibleDigits = Math.floor(maxVisibleDigits / 2);
+	return `${str.substr(0, visibleDigits)}\u2026${str.substr(str.length - visibleDigits)}`;
+});
