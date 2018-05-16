8c19e8be372dbd1e1d78f4198330bee902eead7c
Lucas Silvestre <lukas.silvestre@gmail.com>
2018-05-16 18:49:45 +0200

    Reuse currency filter in currencyFee filter

--- src/filters/currencyFee.js
+++ src/filters/currencyFee.js
@@ -15,26 +15,10 @@
  */
 import AppFilters from './filters.module';
 
-AppFilters.filter('currencyFee', (numberFilter, liskFilter) => (amount, currency, decimalPlacesCrypto, decimalPlacesFiat) => {
-	const lisk = liskFilter(amount);
-	let factor = 1;
-
+AppFilters.filter('currencyFee', currencyFilter => (amount, currency, decimalPlacesCrypto, decimalPlacesFiat) => {
 	if (currency.symbol === 'LSK') {
-		return numberFilter((lisk * factor), 1);
+		decimalPlacesCrypto = 1;
 	}
 
-	if (!decimalPlacesFiat && decimalPlacesFiat !== 0) decimalPlacesFiat = 2;
-
-	if (currency.tickers && currency.tickers.LSK && currency.tickers.LSK[currency.symbol]) {
-		factor = currency.tickers.LSK[currency.symbol];
-	} else if (currency.symbol !== 'LSK') {
-		// Exchange rate not available for current symbol
-		return 'N/A';
-	}
-
-	const decimals = (currency.symbol === 'LSK' || currency.symbol === 'BTC') ? decimalPlacesCrypto : decimalPlacesFiat;
-	if (typeof decimals === 'number' && lisk > 0) {
-		return numberFilter((lisk * factor), decimals);
-	}
-	return numberFilter((lisk * factor), 8).replace(/\.?0+$/, '');
+	return currencyFilter(amount, currency, decimalPlacesCrypto, decimalPlacesFiat);
 });
