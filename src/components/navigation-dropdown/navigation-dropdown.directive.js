79f66d6caf27d786eb0cc64b05e40a5f64911596
Michal Tuleja <michal@lightcurve.io>
2018-06-27 11:39:51 +0200

    Fix for the mobile view

---
+++ src/components/navigation-dropdown/navigation-dropdown.directive.js
@@ -0,0 +1,34 @@
+/*
+ * LiskHQ/lisk-explorer
+ * Copyright © 2018 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+import AppNavDropdown from './navigation-dropdown.module';
+import template from './navigation-dropdown.html';
+
+AppNavDropdown.directive('navigationDropdown', ($rootScope, $timeout) => {
+	const CurrencySelectorLink = () => {
+	};
+
+	const CurrencySelectorCtrl = function ($scope) {
+	};
+
+	return {
+		restrict: 'E',
+		replace: true,
+		controller: CurrencySelectorCtrl,
+		controllerAs: 'cs',
+		link: CurrencySelectorLink,
+		template,
+	};
+});
---
+++ src/components/rounding-selector/rounding-selector.directive.js
@@ -0,0 +1,38 @@
+/*
+ * LiskHQ/lisk-explorer
+ * Copyright © 2018 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+import AppRounding from './rounding-selector.module';
+import template from './rounding-selector.html';
+
+AppRounding.directive('roundingSelector', ($rootScope, $timeout) => {
+	const RoundingSelectorLink = () => {
+	};
+
+	const RoundingSelectorCtrl = function ($scope) {
+		this.setDecimalPlaces = (setting) => {
+			$rootScope.decimalPlaces = setting;
+			$rootScope.isCollapsed = true;
+		};
+	};
+
+	return {
+		restrict: 'E',
+		replace: true,
+		controller: RoundingSelectorCtrl,
+		controllerAs: 'cs',
+		link: RoundingSelectorLink,
+		template,
+	};
+});
