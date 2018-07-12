b7a443399010519ced94438261780622832f6c7e
Lucas Silvestre <lukas.silvestre@gmail.com>
2018-07-12 12:09:24 +0200

    Add docker setup for E2E tests

---
+++ docker-test-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash  
+# Purpose of this script is to clean lisk database and load a snapshot to it
+
+if [ ! -f blockchain_explorer.db.gz ]; then
+  wget https://downloads.lisk.io/lisk-explorer/dev/blockchain_explorer.db.gz
+fi
+
+docker-compose stop lisk-core
+docker-compose start db
+docker-compose run --rm db-task dropdb --if-exists lisk_test
+docker-compose run --rm db-task createdb lisk_test
+gzip --decompress --to-stdout blockchain_explorer.db.gz |docker-compose run --rm db-task psql >/dev/null
+docker-compose start lisk-core
