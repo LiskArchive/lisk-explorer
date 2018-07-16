51198e7be84cd803efc1c40986c8f44e82301e67
Michal Tuleja <michal@lightcurve.io>
2018-07-16 10:02:33 +0200

    Last twitter status in top bar

--- src/components/information-banner/information-banner.directive.js
+++ src/components/information-banner/information-banner.directive.js
@@ -17,12 +17,23 @@ import ParseMarkdownJs from 'parse-markdown-js';
 import InformationBanner from './information-banner.module';
 import template from './information-banner.html';
 
+// const parseTweet = (message) => {};
+
 const directiveCtrl = ($scope, $http, $sce) => {
 	$scope.text = '';
 
 	$http.get('/api/ui_message').then((result) => {
 		if (result.data.success && result.data.content) {
 			$scope.text = $sce.trustAsHtml(ParseMarkdownJs(result.data.content));
+		} else {
+			$http.get('/api/newsfeed').then((result) => {
+				if (Array.isArray(result.data) && result.data.length > 0) {
+					const topMessage = result.data.sort((a, b) => a.timestamp < b.timestamp)[0];
+					const message = topMessage.content.replace(topMessage.url, '');
+					const link = `[Read more](${topMessage.url})`;
+					$scope.text = $sce.trustAsHtml([ParseMarkdownJs('[Twitter/LiskHQ](https://twitter.com/LiskHQ)'), '--', message, ParseMarkdownJs(link)].join(' '));
+				}
+			});
 		}
 	});
 };
