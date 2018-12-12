caeb20d94822362529f8ed3e2bc387adc8492c39
Michal Tuleja <michal@lightcurve.io>
2018-12-12 20:26:50 +0100

    New implementation of transaction filter

--- src/shared/transactions-filter/transactions-filter.directive.js
+++ src/shared/transactions-filter/transactions-filter.directive.js
@@ -16,108 +16,80 @@
 import AppTools from '../../app/app-tools.module';
 import template from './transactions-filter.html';
 
-const TransactionsConstructor = function ($rootScope, $scope, $timeout, allTxs) {
+const TransactionsConstructor = function ($rootScope, $scope, $stateParams, $element, $state) {
 	$scope.searchModel = [];
 	$scope.searchParams = [];
 	$scope.availableSearchParams = [
-		{ key: 'senderId', name: 'Sender ID', placeholder: 'Sender...' },
-		{ key: 'senderPublicKey', name: 'Sender Public Key', placeholder: 'Sender Public Key...' },
-		{ key: 'recipientId', name: 'Recipient ID', placeholder: 'Recipient...' },
-		{ key: 'recipientPublicKey', name: 'Recipient Public Key', placeholder: 'Recipient Public Key...' },
-		{ key: 'minAmount', name: 'Min Amount', placeholder: 'Min Amount...' },
-		{ key: 'maxAmount', name: 'Max Amount', placeholder: 'Max Amount...' },
-		{ key: 'type', name: 'Transaction Type', placeholder: 'Comma separated...' },
-		{ key: 'height', name: 'Block Height', placeholder: 'Block Height...' },
-		{ key: 'blockId', name: 'Block Id', placeholder: 'Block Id...' },
-		{ key: 'fromTimestamp', name: 'From Timestamp', placeholder: 'From Timestamp...' },
-		{ key: 'toTimestamp', name: 'To Timestamp', placeholder: 'To Timestamp...' },
-		{ key: 'limit', name: 'Limit', placeholder: 'Limit...' },
-		{ key: 'offset', name: 'Offset', placeholder: 'Offset...' },
-		{
-			key: 'sort',
-			name: 'Order By',
-			placeholder: 'Order By...',
-			restrictToSuggestedValues: true,
-			suggestedValues: ['amount:asc', 'amount:desc', 'fee:asc', 'fee:desc', 'type:asc', 'type:desc', 'timestamp:asc', 'timestamp:desc'],
-		},
+		{ key: 'senderId', name: 'Sender ID', placeholder: 'Sender...', example: '12317412804123L' },
+		{ key: 'senderPublicKey', name: 'Sender Public Key', placeholder: 'Sender Public Key...', example: 'b550ede5...a26c78d8' },
+		{ key: 'recipientId', name: 'Recipient ID', placeholder: 'Recipient...', example: '12317412804123L' },
+		{ key: 'recipientPublicKey', name: 'Recipient Public Key', placeholder: 'Recipient Public Key...', example: 'b550ede5...a26c78d8' },
+		{ key: 'minAmount', name: 'Min Amount', placeholder: 'Min Amount...', example: '1.25' },
+		{ key: 'maxAmount', name: 'Max Amount', placeholder: 'Max Amount...', example: '1000.5' },
+		{ key: 'type', name: 'Comma separated transaction types', placeholder: 'Comma separated...', example: '1,3' },
+		{ key: 'height', name: 'Block height', placeholder: 'Block Height...', example: '2963014' },
+		{ key: 'blockId', name: 'Block Id', placeholder: 'Block Id...', example: '17238091754034756025' },
+		{ key: 'fromTimestamp', name: 'From Timestamp', placeholder: 'From Timestamp...', example: '17238091754034756025' },
+		{ key: 'toTimestamp', name: 'To Timestamp', placeholder: 'To Timestamp...', example: '12317412804123L' },
+		// { key: 'limit', name: 'Limit', placeholder: 'Limit...', example: '12317412804123L' },
+		// { key: 'offset', name: 'Offset', placeholder: 'Offset...', example: '12317412804123L' },
+		// {
+		// 	key: 'sort',
+		// 	name: 'Order By',
+		// 	placeholder: 'Order By...',
+		// 	restrictToSuggestedValues: true,
+		// 	suggestedValues:
+		//     ['amount:asc', 'amount:desc', 'fee:asc', 'fee:desc', 'type:asc',
+		//      'type:desc', 'timestamp:asc', 'timestamp:desc'],
+		// },
 	];
 
+	$scope.queryBoxActive = false;
+	// $scope.queryHelpActive = true;
+
+	const convertToUrl = (key, param) => {
+		switch (key) {
+		case 'minAmount':
+		case 'maxAmount':
+			return Number(param) * Math.pow(10, 8);
+		default:
+			return param;
+		}
+	};
+
+	const convertFromUrl = (key, param) => {
+		switch (key) {
+		case 'minAmount':
+		case 'maxAmount':
+			return Number(param) / Math.pow(10, 8);
+		default:
+			return param;
+		}
+	};
+
+	$scope.addToQueryText = (key) => {
+		$scope.queryText += ` ${key}=`;
+	};
+
+	$scope.performSearch = () => {
+		const query = $scope.queryText.split(' ')
+			.map(param => param.split('='))
+			.reduce((acc, param) => {
+				acc[param[0]] = convertToUrl(param[0], param[1]);
+				return acc;
+			}, {});
+		$state.go($state.current.component, Object.assign({ page: 1 }, query), { inherit: false });
+	};
+
+	$scope.queryText = Object.keys($stateParams)
+		.filter(key => key !== 'page')
+		.filter(key => key !== 'address')
+		.filter(key => key !== '#')
+		.filter(key => typeof $stateParams[key] !== 'undefined')
+		.map(key => `${key}=${convertFromUrl(key, $stateParams[key])}`)
+		.join(' ');
+
 	$scope.parametersDisplayLimit = $scope.availableSearchParams.length;
-
-	const onSearchBoxCleaned = () => {
-		if ($scope.cleanByFilters) {
-			$scope.cleanByFilters = false;
-		} else {
-			$scope.invalidParams = false;
-			$scope.txs = allTxs();
-			$scope.txs.loadData();
-		}
-	};
-
-	const searchByParams = (params) => {
-		if ($scope.direction !== 'search') {
-			$scope.lastDirection = $scope.direction;
-			$scope.direction = 'search';
-		}
-		$scope.invalidParams = false;
-		$scope.txs = allTxs(params);
-		$scope.txs.loadData();
-	};
-
-	const onSearchChange = () => {
-		const params = {};
-		Object.keys($scope.searchModel).forEach((key) => {
-			if ($scope.searchModel[key] !== undefined && $scope.searchModel[key] !== '') {
-				params[key] = $scope.searchModel[key];
-			}
-			if ((key === 'minAmount' || key === 'maxAmount') && params[key] !== '') {
-				params[key] = Math.floor(parseFloat(params[key]) * 1e8);
-			}
-		});
-
-		if (params.query) {
-			params.senderId = params.query;
-			params.recipientId = params.query;
-		}
-
-		if (Object.keys(params).length > 0) {
-			searchByParams(params);
-		} else {
-			onSearchBoxCleaned();
-		}
-	};
-
-	$rootScope.$on('advanced-searchbox:modelUpdated', (event, model) => {
-		if ($scope.searchModel.query !== model.query) {
-			$scope.searchModel = Object.assign({}, model);
-			return onSearchChange();
-		}
-
-		return $scope.searchModel = Object.assign({}, model);
-	});
-
-	$rootScope.$on('advanced-searchbox:removedSearchParam', (event, searchParameter) => {
-		delete $scope.searchModel[searchParameter.key];
-		onSearchChange();
-	});
-
-	$rootScope.$on('advanced-searchbox:removedAllSearchParam', () => {
-		onSearchBoxCleaned();
-	});
-
-	$rootScope.$on('advanced-searchbox:leavedEditMode', (event, searchParameter) => {
-		$scope.searchModel[searchParameter.key] = searchParameter.value;
-		onSearchChange();
-	});
-
-	// Sets autocomplete attr off
-	const disableAutocomplete = () => {
-		$timeout(() => {
-			document.getElementsByClassName('search-parameter-input')[0].setAttribute('autocomplete', 'off');
-		}, 0);
-	};
-
-	disableAutocomplete();
 };
 
 const transactionsFilter = AppTools.directive('transactionsFilter', () => ({
