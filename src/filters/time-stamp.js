fdf882c0072f83eb259810d62ef2479ffef72ce3
Michal Tuleja <michal@lightcurve.io>
2020-01-24 13:25:30 +0100

    Timestamp format fix

--- src/filters/time-stamp.js
+++ src/filters/time-stamp.js
@@ -13,40 +13,9 @@
  * Removal or modification of this copyright notice is prohibited.
  *
  */
+import moment from 'moment';
 import AppFilters from './filters.module';
 
-AppFilters.filter('timestamp', epochStampFilter => (timestamp) => {
-	const stamp = epochStampFilter(timestamp);
-	let month = stamp.getMonth() + 1;
-
-	if (month < 10) {
-		month = `0${month}`;
-	}
-
-	let day = stamp.getDate();
-
-	if (day < 10) {
-		day = `0${day}`;
-	}
-
-	let hours = stamp.getHours();
-	let minutes = stamp.getMinutes();
-	let seconds = stamp.getSeconds();
-
-	/**
-	 * @todo use zeroFill instead
-	 */
-	if (hours < 10) {
-		hours = `0${hours}`;
-	}
-
-	if (minutes < 10) {
-		minutes = `0${minutes}`;
-	}
-
-	if (seconds < 10) {
-		seconds = `0${seconds}`;
-	}
-
-	return `${stamp.getFullYear()}/${month}/${day} ${hours}:${minutes}:${seconds}`;
-});
+AppFilters.filter('timestamp', epochStampFilter =>
+	timestamp => moment(epochStampFilter(timestamp))
+		.format('YYYY/MM/DD, HH:mm:ss'));
